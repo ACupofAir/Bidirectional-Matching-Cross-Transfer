3891878d9f865c82e8e16f4c1d4486f6d81bd9fc
ACupofAir <thoughts.times@gmail.com>
2024-10-12 23:05:30 +0800

    [UPDATE] train ui update

--- Deit_Cross_Att/train_ui.py
+++ Deit_Cross_Att/train_ui.py
@@ -8,7 +8,6 @@ from PyQt5.QtWidgets import (
     QHBoxLayout,
     QSpinBox,
     QLabel,
-    QComboBox,
     QPushButton,
     QTextEdit,
     QMessageBox,
@@ -63,9 +62,6 @@ class TrainInterface(QWidget):
 
         # First Module: Input Parameters
         model_label = QLabel("模型参数:")
-        self.model_combobox = QComboBox()
-        self.model_combobox.addItems(["deit_base", "deit_small"])
-
         epochs_label = QLabel("epochs:")
         self.epochs_spinbox = QSpinBox()
         self.epochs_spinbox.setRange(1, 1000)  # Set range for epochs
@@ -89,22 +85,22 @@ class TrainInterface(QWidget):
         self.transfer_button = AirBtn("迁移", fixed_size=(120, 40))
         self.transfer_button.clicked.connect(self.run_transfer_script)
 
-        input_params_layout.addWidget(model_label)
-        input_params_layout.addWidget(self.model_combobox)
-
         epochs_layout = QHBoxLayout()
         epochs_layout.addWidget(epochs_label)
         epochs_layout.addWidget(self.epochs_spinbox)
-        input_params_layout.addLayout(epochs_layout)
 
         source_file_layout = QHBoxLayout()
         source_file_layout.addWidget(self.source_file_display)
         source_file_layout.addWidget(self.source_file_button)
-        input_params_layout.addLayout(source_file_layout)
 
         target_file_layout = QHBoxLayout()
         target_file_layout.addWidget(self.target_file_display)
         target_file_layout.addWidget(self.target_file_button)
+
+        # input params: model_label, epochs_layout, source_file_layout, target_file_layout
+        input_params_layout.addWidget(model_label)
+        input_params_layout.addLayout(epochs_layout)
+        input_params_layout.addLayout(source_file_layout)
         input_params_layout.addLayout(target_file_layout)
 
         buttons_layout.addWidget(self.train_button)
@@ -116,11 +112,11 @@ class TrainInterface(QWidget):
         # Second Module: Script Output
         self.output_text = QTextEdit()
         self.output_text.setReadOnly(True)
-        output_layout.addWidget(QLabel("脚本输出:"))
-        output_layout.addWidget(self.output_text)
         self.figure = Figure()
         self.canvas = FigureCanvas(self.figure)
         output_layout.addWidget(self.canvas)
+        output_layout.addWidget(QLabel("日志输出:"))
+        output_layout.addWidget(self.output_text)
 
         # Third Module: Final Accuracy and Export Model
         self.accuracy_info_box = InfoShowWidget("准确率:", "0%")
@@ -192,7 +188,6 @@ class TrainInterface(QWidget):
             self.target_file_display.setText(file)
 
     def run_train_script(self):
-        model = self.model_combobox.currentText()
         epochs = self.epochs_spinbox.value()
         if not self.source_file_path or not self.target_file_path:
             QMessageBox.critical(self, "错误", "请提供源域和目标域文件路径")
@@ -200,7 +195,7 @@ class TrainInterface(QWidget):
 
         cmd = (
             f"python train.py  --config_file configs/pretrain.yml DATASETS.NAMES Shipsear "
-            f'OUTPUT_DIR "../logs/pretrain/{model}/shipsear/target" '
+            f'OUTPUT_DIR "../logs/pretrain/deit_base/shipsear/target" '
             f'DATASETS.ROOT_TRAIN_DIR "{self.source_file_path}" '
             f'DATASETS.ROOT_TEST_DIR "{self.target_file_path}" '
             f"SOLVER.LOG_PERIOD 10 "
@@ -212,7 +207,6 @@ class TrainInterface(QWidget):
 
     def run_transfer_script(self):
         # Implement the transfer script logic here
-        model = self.model_combobox.currentText()
         epochs = self.epochs_spinbox.value()
         if not self.source_file_path or not self.target_file_path:
             QMessageBox.critical(self, "错误", "请提供源域和目标域文件路径")
@@ -222,7 +216,7 @@ class TrainInterface(QWidget):
             f"python train.py  --config_file configs/uda.yml "
             f"DATASETS.NAMES Shipsear "
             f"DATASETS.NAMES2 Shipsear "
-            f'OUTPUT_DIR "../logs/pretrain/{model}/shipsear/target" '
+            f'OUTPUT_DIR "../logs/pretrain/deit_base/shipsear/target" '
             f'MODEL.PRETRAIN_PATH "../logs/pretrain/deit_base/shipsear/target/transformer_10.pth" '
             f'DATASETS.ROOT_TRAIN_DIR "{self.source_file_path}" '
             f'DATASETS.ROOT_TRAIN_DIR2 "{self.target_file_path}" '
@@ -237,10 +231,10 @@ class TrainInterface(QWidget):
     def update_plot(self):
         self.figure.clear()
         ax = self.figure.add_subplot(121)
-        ax.plot(self.loss_data, label="Loss")
+        ax.plot(self.acc_data, label="Accuracy")
         ax.legend()
         ax = self.figure.add_subplot(122)
-        ax.plot(self.acc_data, label="Accuracy")
+        ax.plot(self.loss_data, label="Loss", color="red")
         ax.legend()
         self.canvas.draw()
 
@@ -250,10 +244,6 @@ class TrainInterface(QWidget):
         self.output_text.append(stdout)
         loss_match = re.search(r"Loss:\s*([\d.]+)", stdout)
         acc_match = re.search(r"Acc:\s*([\d.]+)", stdout)
-        print("======================DEBUG START: loss acc match======================")
-        print(loss_match)
-        print(acc_match)
-        print("======================DEBUG  END : loss acc match======================")
         if loss_match and acc_match:
             loss = float(loss_match.group(1))
             acc = float(acc_match.group(1)) * 100
@@ -269,22 +259,12 @@ class TrainInterface(QWidget):
 
     def process_finished(self):
         self.timer.stop()
-        print(
-            "======================DEBUG START: process finished======================"
-        )
         QMessageBox.information(self, "成功", "脚本执行完成")
-        print(
-            "======================DEBUG  END : process finished======================"
-        )
 
     def stop_training(self):
         if self.process.state() == QProcess.Running:
             self.process.kill()
             QMessageBox.information(self, "成功", "脚本执行完成")
-            print("======================DEBUG START: acc loss======================")
-            print(self.acc_data)
-            print(self.loss_data)
-            print("======================DEBUG  END : acc loss======================")
 
     def export_model(self):
         # Implement the export model logic here
